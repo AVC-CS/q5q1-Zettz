c902298fd0f03ae2c66143a7f12698bcee328cde
Lorenzo <calderonlorenzo026@gmail.com>
2026-04-13 20:09:03 +0000

    added q5q1 code

--- main.cpp
+++ main.cpp
@@ -5,15 +5,19 @@ int main()
 {
     int n;
     // TODO: ask user for input
+    cout << "Input a number: ";
+    cin >> n;
 
     // TODO: outer loop for each row
-
-        // TODO: inner loop to print letters for this row
-
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             // TODO: print the letter for this column
-
-        // TODO: print newline after each row
-
-
+            cout << char('A' + j - 1) << " ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
